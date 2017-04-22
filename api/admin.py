c90dc61263ec598eeed6410daa6da9c38caec750
risatrix <amandakrauss@Amanda-Krausss-iMac-2.local>
2017-04-22 08:37:46 -0500

    add models to admin, add gitignore

--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from api.models import Author, Work, Book, Poem, Line
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Work)
+admin.site.register(Book)
+admin.site.register(Poem)
+admin.site.register(Line)
