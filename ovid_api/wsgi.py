c59f64983add466bae577653f2da565c783f5e87
risatrix <amandakrauss@Amanda-Krausss-iMac-2.local>
2017-04-30 16:56:00 -0500

    add .base to wsgi settings call

--- ovid_api/wsgi.py
+++ ovid_api/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ovid_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ovid_api.settings.base")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
