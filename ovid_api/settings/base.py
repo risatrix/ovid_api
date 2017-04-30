c8484c98684fbb0dee27d5e9fd31ca143308daa8
risatrix <amandakrauss@Amanda-Krausss-iMac-2.local>
2017-04-30 16:36:28 -0500

    change staticfiles dir

--- ovid_api/settings/base.py
+++ ovid_api/settings/base.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # REST framework stuff
